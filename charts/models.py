4de42719a55c2fae4fba0495b065e6704e0fe2b4
marshl <marshl@live.com.au>
2017-10-12 22:12:27 +1100

    Added the Repository and Commit models

--- charts/models.py
+++ charts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Repository(models.Model):
+    title = models.CharField(max_length=150)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Commit(models.Model):
+    repo = models.ForeignKey(Repository)
+    author = models.CharField(max_length=150)
+    message = models.TextField()
+    hex_sha = models.CharField(max_length=40)
+    bin_sha = models.BinaryField(max_length=20)
+
+    authored_datetime = models.DateTimeField()
+    committed_datetime = models.DateTimeField()
+
+    count = models.IntegerField()
+
+    line_additions = models.IntegerField()
+    line_subtractions = models.IntegerField()
+
+    def __str__(self):
+        return self.hex_sha
