0573ae66a07cd3a82ec597a6549b5ad3e74ad134
marshl <marshl@live.com.au>
2017-10-14 20:03:30 +1100

    Added bootstrap

--- charts/views.py
+++ charts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Repository
+from django.utils import timezone
 
 
 # Create your views here.
 def repo_list(request):
-    return render(request, 'charts/repo_list.html', {})
+    repos = Repository.objects.all().order_by('title')
+    return render(request, 'charts/repo_list.html', {'repos': repos})
