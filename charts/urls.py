4c318ced97e6441689bfb7e493723f95ceed59b1
marshl <marshl@live.com.au>
2017-10-14 20:32:06 +1100

    Added detail pages for repositories

--- charts/urls.py
+++ charts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.repo_list, name='repo_list'),
+    url(r'^repo/(?P<pk>\d+)/$', views.repo_detail, name='repo_detail'),
 ]
--- charts/views.py
+++ charts/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Repository
-from django.utils import timezone
 
 
 # Create your views here.
 def repo_list(request):
     repos = Repository.objects.all().order_by('title')
     return render(request, 'charts/repo_list.html', {'repos': repos})
+
+
+def repo_detail(request, pk):
+    repo = get_object_or_404(Repository, pk=pk)
+    return render(request, 'charts/repo_detail.html', {'repo': repo})
