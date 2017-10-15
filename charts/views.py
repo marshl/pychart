61c12e5dc64acd9cbf57f33f9e680c06d912b028
marshl <marshl@live.com.au>
2017-10-15 14:59:36 +1100

    Changed the pie chart to get derive data from the database

--- charts/views.py
+++ charts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import JsonResponse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from .models import Repository
 
 
@@ -27,22 +30,33 @@ def repo_reset(request, pk):
 
 
 def get_chart_data(request):
-
-    
-
+    repo = Repository.objects.all()[1]
+    # authors = []
+    # for c in repo.commit_set.all():
+    #     authors.append(c.author)
+    #
+    # print(authors)
+    authors = pd.Series([x.author for x in repo.commit_set.all()])
+    df = pd.DataFrame({'count': 1, 'author': authors})
+    series = df.groupby('author')['count'].sum()
 
     result = {
         "cols": [
-            {"id": "", "label": "Topping", "pattern": "", "type": "string"},
-            {"id": "", "label": "Slices", "pattern": "", "type": "number"}
+            {"id": "", "label": "Author", "pattern": "", "type": "string"},
+            {"id": "", "label": "Commits", "pattern": "", "type": "number"}
         ],
-        "rows": [
-            {"c": [{"v": "Mushrooms", "f": None}, {"v": 3, "f": None}]},
-            {"c": [{"v": "Onions", "f": None}, {"v": 1, "f": None}]},
-            {"c": [{"v": "Olives", "f": None}, {"v": 1, "f": None}]},
-            {"c": [{"v": "Zucchini", "f": None}, {"v": 1, "f": None}]},
-            {"c": [{"v": "Pepperoni", "f": None}, {"v": 2, "f": None}]}
-        ]
+        'rows': series_to_rows(series)
+        # "rows": [
+        #     {"c": [{"v": "Andrew Pigram", "f": None}, {"v": 27, "f": None}]},
+        #     {"c": [{"v": "Liam Marshall", "f": None}, {"v": 160, "f": None}]},
+        #     {"c": [{"v": "Vegemash", "f": None}, {"v": 9, "f": None}]},
+        #     {"c": [{"v": "prathyusha sama", "f": None}, {"v": 4, "f": None}]},
+        # ]
     }
 
     return JsonResponse(result, safe=False)
+
+
+def series_to_rows(series: pd.Series):
+    d = series.to_dict()
+    return [{'c': [{'v': x}, {'v': int(d[x])}]} for x in d]
