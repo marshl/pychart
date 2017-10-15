2cb227c5db6669485560f1edeefa95634cf91dc3
marshl <marshl@live.com.au>
2017-10-15 23:15:52 +1100

    Added a graph for commits per day

--- charts/urls.py
+++ charts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^repo/(?P<pk>\d+)/reload$', views.repo_reload, name='repo_reload'),
     url(r'^repo/(?P<pk>\d+)/reset', views.repo_reset, name='repo_reset'),
     url(r'^repo/(?P<pk>\d+)/get_repo_author_total', views.get_repo_author_total, name='get_repo_author_total'),
+    url(r'^repo/(?P<pk>\d+)/get_commits_per_day', views.get_commits_per_day, name='get_commits_per_day'),
 ]
--- charts/views.py
+++ charts/views.py
@@ -33,13 +33,39 @@ def get_repo_author_total(request, pk):
     repo = get_object_or_404(Repository, pk=pk)
     authors = pd.Series([x.author for x in repo.commit_set.all()])
     series = authors.value_counts()
+    d = series.to_dict()
 
     result = {
         "cols": [
             {"id": "", "label": "Author", "pattern": "", "type": "string"},
             {"id": "", "label": "Commits", "pattern": "", "type": "number"}
         ],
-        'rows': series_to_rows(series)
+        'rows':
+            [{'c': [
+                {'v': x}, {'v': int(d[x])}
+            ]} for x in d]
+    }
+
+    return JsonResponse(result, safe=False)
+
+
+def get_commits_per_day(request, pk):
+    repo = get_object_or_404(Repository, pk=pk)
+    commits = pd.Series([x.committed_datetime.date() for x in repo.commit_set.all()])
+    # series = commits.value_counts()
+    df = pd.DataFrame({'commit_date': commits})
+    series = df.groupby('commit_date').size().asfreq('1d', fill_value=0)
+    d = series.to_dict()
+
+    result = {
+        "cols": [
+            {"id": "", "label": "Date", "pattern": "", "type": "date"},
+            {"id": "", "label": "Commits Per Day", "pattern": "", "type": "number"}
+        ],
+        'rows':
+            [{'c': [
+                {'v': date_to_json(x)}, {'v': int(d[x])}
+            ]} for x in d]
     }
 
     return JsonResponse(result, safe=False)
@@ -47,4 +73,8 @@ def get_repo_author_total(request, pk):
 
 def series_to_rows(series: pd.Series):
     d = series.to_dict()
-    return [{'c': [{'v': x}, {'v': int(d[x])}]} for x in d]
+    return
+
+
+def date_to_json(date):
+    return date.strftime(f'Date(%Y, %m, %d, 0, 0, 0)')
